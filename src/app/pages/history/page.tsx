abc4386d3b94c7cc3fa7a926cb1b3a117209fdf7
lorenzo-mignola <lo.mignola@gmail.com>
2023-10-03 18:22:18 +0200

    fix: history page

--- src/app/pages/history/page.tsx
+++ src/app/pages/history/page.tsx
@@ -11,7 +11,7 @@ export default function History() {
         <Trans
           // eslint-disable-next-line react/jsx-key, jsx-a11y/heading-has-content -- not needed
           components={[<h2 className="pt-2 text-lg font-bold" />, <p />]}
-          i18nKey="training:body"
+          i18nKey="history:body"
         />
       </article>
     </>
