dac6a3b250f3dc72a368301589e267f11323d438
lorenzo-mignola <lo.mignola@gmail.com>
2023-10-07 11:23:56 +0200

    feat: update home text

--- src/components/Sections/section-atls.tsx
+++ src/components/Sections/section-atls.tsx
@@ -1,17 +1,22 @@
 import useTranslation from "next-translate/useTranslation";
+import Image from "next/image";
 import { SectionHeader } from "./section-header";
 import { SectionParagraph } from "./section-paragraph";
+import SectionImage from "@/public/images/section-atls.jpg";
 
 export function SectionATLS() {
   const { t } = useTranslation("home");
   return (
     <section
-      className="bg-neutral p-5 text-neutral-content lg:p-20"
+      className="bg-neutral p-5 text-neutral-content lg:flex lg:items-center lg:p-20"
       id="section-atls"
     >
-      <SectionHeader>{t("section-atls-header")}</SectionHeader>
-      <SectionParagraph>{t("section-atls-body-1")}</SectionParagraph>
-      <SectionParagraph>{t("section-atls-body-2")}</SectionParagraph>
+      <div>
+        <SectionHeader>{t("section-atls-header")}</SectionHeader>
+        <SectionParagraph>{t("section-atls-body-1")}</SectionParagraph>
+        <SectionParagraph>{t("section-atls-body-2")}</SectionParagraph>
+      </div>
+      <Image alt="Lotta svizzera" className="lg:ml-4" src={SectionImage} />
     </section>
   );
 }
--- src/components/Sections/section-lotta.tsx
+++ src/components/Sections/section-lotta.tsx
@@ -1,14 +1,22 @@
 import useTranslation from "next-translate/useTranslation";
+import Image from "next/image";
 import { SectionHeader } from "./section-header";
 import { SectionParagraph } from "./section-paragraph";
+import SectionImage from "@/public/images/section-lotta.jpg";
 
 export function SectionLotta() {
   const { t } = useTranslation("home");
   return (
-    <section className="bg-base-100 p-5 text-right lg:p-20" id="section">
-      <SectionHeader>{t("section-lotta-header")}</SectionHeader>
-      <SectionParagraph>{t("section-lotta-body-1")}</SectionParagraph>
-      <SectionParagraph>{t("section-lotta-body-2")}</SectionParagraph>
+    <section
+      className="flex flex-col-reverse bg-base-100 p-5 lg:flex-row lg:items-center lg:p-20"
+      id="section"
+    >
+      <Image alt="Lotta svizzera" className="lg:mr-4" src={SectionImage} />
+      <div>
+        <SectionHeader>{t("section-lotta-header")}</SectionHeader>
+        <SectionParagraph>{t("section-lotta-body-1")}</SectionParagraph>
+        <SectionParagraph>{t("section-lotta-body-2")}</SectionParagraph>
+      </div>
     </section>
   );
 }
