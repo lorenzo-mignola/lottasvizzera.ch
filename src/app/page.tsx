56994dd8802c8bdae638c79153730f52dcb6ccb6
lorenzo-mignola <lo.mignola@gmail.com>
2024-03-03 16:50:19 +0100

    chore: add cookie banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import { Hero } from "@components/Hero/hero";
+import Script from "next/script";
 import { Sections } from "../components/Sections/sections";
 
 export default function Home() {
   return (
-    <div>
-      <Hero />
-      <Sections />
-    </div>
+    <>
+      <Script
+        data-blockingmode="auto"
+        data-cbid="bed08fbc-ff04-453f-a31c-a5681bd9a7f0"
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        type="text/javascript"
+      />
+      <div>
+        <Hero />
+        <Sections />
+      </div>
+    </>
   );
 }
