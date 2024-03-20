6e41a982096504e65f2134bf11862da218a7541b
lorenzo-mignola <lo.mignola@gmail.com>
2024-03-20 19:03:11 +0100

    fix: home icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,21 @@ export default function RootLayout({
 }) {
   return (
     <html data-theme="autumn" lang="it">
+      <head>
+        <link href="/favicon.ico" rel="icon" sizes="any" />
+        <link
+          href="/icon?<generated>"
+          rel="icon"
+          sizes="<generated>"
+          type="image/<generated>"
+        />
+        <link
+          href="/apple-icon?<generated>"
+          rel="apple-touch-icon"
+          sizes="<generated>"
+          type="image/<generated>"
+        />
+      </head>
       <body
         className={`${inter.className} md:flex md:min-h-screen md:flex-col`}
       >
